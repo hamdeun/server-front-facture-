4070d8d24428c377e22d25700b068f062c583179
hamdeun <77253291+hamdeun@users.noreply.github.com>
2024-01-17 03:57:15 +0100

    f

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  backendHost:"http://localhost:3000"
+  production: true,
+  backendHost:"https://apifact.paperslab.tn"
 };
 
 /*
